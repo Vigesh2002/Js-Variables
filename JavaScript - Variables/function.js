d819596f82a5f36ecdb268c27e071979734ced17
Vigesh D <105991790+Vigesh2002@users.noreply.github.com>
2023-08-19 00:10:04 +0530

    Add files via upload

---
+++ JavaScript - Variables/function.js
@@ -0,0 +1,45 @@
+// Variables are Containers for Storing Data
+// JavaScript Variables can be declared in 4 ways:
+
+// Automatically
+// Using var
+// Using let
+// Using const
+
+
+// Automatically Declare
+
+x = 5;
+y = 6;
+z = x + y;
+
+document.getElementById("variable").innerHTML = z;
+
+
+
+
+// Using var
+
+var a = 10;
+var b = 20;
+var c = a + b;
+
+document.getElementById("one").innerHTML = c;
+
+
+// Using let
+
+let d = 56;
+let e = 27;
+let f = d + e;
+
+document.getElementById("two").innerHTML = f;
+
+
+// Using const
+
+const g = 94;
+const h = 67;
+const i = g + h;
+
+document.getElementById("three").innerHTML = i;
